d63ef39b2eb9eafe1773637d6f3ff7974eb933ac
pmgespina <pmgespina@gmail.com>
2024-12-02 07:28:42 +0000

    Runner Algorithm for Integer Biclusters

--- jmetal-algorithm/src/main/java/org/uma/jmetal/algorithm/examples/singleobjective/geneticalgorithm/GenerationalGeneticAlgorithmIntegerBiclusterRunner.java
+++ jmetal-algorithm/src/main/java/org/uma/jmetal/algorithm/examples/singleobjective/geneticalgorithm/GenerationalGeneticAlgorithmIntegerBiclusterRunner.java
@@ -6,25 +6,18 @@ import java.util.List;
 import org.uma.jmetal.algorithm.Algorithm;
 import org.uma.jmetal.algorithm.examples.AlgorithmRunner;
 import org.uma.jmetal.algorithm.singleobjective.geneticalgorithm.GeneticAlgorithmBuilder;
-import org.uma.jmetal.operator.crossover.CrossoverOperator;
 import org.uma.jmetal.operator.crossover.impl.IntegerBiclusterCrossover;
-import org.uma.jmetal.operator.crossover.impl.SinglePointCrossover;
-import org.uma.jmetal.operator.mutation.MutationOperator;
-import org.uma.jmetal.operator.mutation.impl.BitFlipMutation;
 import org.uma.jmetal.operator.mutation.impl.IntegerBiclusterMutation;
 import org.uma.jmetal.operator.selection.SelectionOperator;
-import org.uma.jmetal.operator.selection.impl.BinaryTournamentSelection;
-import org.uma.jmetal.problem.Problem;
+import org.uma.jmetal.operator.selection.impl.RandomSelection;
 import org.uma.jmetal.problem.singleobjective.IntegerBiclustering;
-import org.uma.jmetal.solution.binarysolution.BinarySolution;
 import org.uma.jmetal.solution.compositesolution.IntegerCompositeSolution;
 import org.uma.jmetal.util.JMetalLogger;
 import org.uma.jmetal.util.NormalizeUtils;
 import org.uma.jmetal.util.fileoutput.SolutionListOutput;
 import org.uma.jmetal.util.fileoutput.impl.DefaultFileOutputContext;
 import org.uma.jmetal.util.genedataloader.GeneDataLoader;
-import org.uma.jmetal.util.pseudorandom.JMetalRandom;
-import org.uma.jmetal.util.pseudorandom.RandomGenerator;
+import org.uma.jmetal.util.pseudorandom.impl.JavaRandomGenerator;
 /**
  * Class to configure and run a generational genetic algorithm. The target problem is OneMax.
  *
@@ -39,21 +32,21 @@ public class GenerationalGeneticAlgorithmIntegerBiclusterRunner {
     Algorithm<IntegerCompositeSolution> algorithm;
     IntegerBiclusterCrossover crossover;
     IntegerBiclusterMutation mutation;
-    SelectionOperator<List<BinarySolution>, BinarySolution> selection;
+    SelectionOperator<List<IntegerCompositeSolution>, IntegerCompositeSolution> selection;
 
     double[][] matrix = GeneDataLoader.loadGeneExpressionMatrix("/home/khaosdev/jMetalJava/jMetal/resources/fabia_100x1000.csv");
     matrix = NormalizeUtils.normalize(matrix);
 
     problem = new IntegerBiclustering(matrix) ;
 
-    crossover = new IntegerBiclusterCrossover(0.9, new RandomGenerator<Double>(), new RandomGenerator<Integer>()) ;
+    crossover = new IntegerBiclusterCrossover(0.9, new JavaRandomGenerator(1), new JavaRandomGenerator(1)) ;
 
     double mutationProbability = 0.4 ;
-    mutation = new BitFlipMutation(mutationProbability) ;
+    mutation = new IntegerBiclusterMutation(mutationProbability, new JavaRandomGenerator(1), new JavaRandomGenerator(1)) ;
 
-    selection = new BinaryTournamentSelection<>();
+    selection = new RandomSelection<>();
 
-    algorithm = new GeneticAlgorithmBuilder<>(problem, crossover, mutation)
+    algorithm = new GeneticAlgorithmBuilder<IntegerCompositeSolution>(problem, crossover, mutation)
             .setPopulationSize(100)
             .setMaxEvaluations(25000)
             .setSelectionOperator(selection)
@@ -62,8 +55,8 @@ public class GenerationalGeneticAlgorithmIntegerBiclusterRunner {
     AlgorithmRunner algorithmRunner = new AlgorithmRunner.Executor(algorithm)
             .execute() ;
 
-    BinarySolution solution = algorithm.result() ;
-    List<BinarySolution> population = new ArrayList<>(1) ;
+    IntegerCompositeSolution solution = algorithm.result() ;
+    List<IntegerCompositeSolution> population = new ArrayList<>(1) ;
     population.add(solution) ;
 
     long computingTime = algorithmRunner.getComputingTime() ;
