f244458a00e89134931a4562dde03e0f49645e1d
pmgespina <pmgespina@gmail.com>
2024-12-11 13:28:33 +0000

    Adaptation of crossover operator for the new problem definition

--- jmetal-core/src/main/java/org/uma/jmetal/operator/crossover/impl/IntegerBiclusterCrossover.java
+++ jmetal-core/src/main/java/org/uma/jmetal/operator/crossover/impl/IntegerBiclusterCrossover.java
@@ -2,28 +2,34 @@ package org.uma.jmetal.operator.crossover.impl;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import org.uma.jmetal.operator.crossover.CrossoverOperator;
-import org.uma.jmetal.solution.compositesolution.IntegerCompositeSolution;
-import org.uma.jmetal.solution.permutationsolution.impl.IntegerPermutationSolution;
+import org.uma.jmetal.solution.compositesolution.CompositeSolution;
+import org.uma.jmetal.solution.integersolution.IntegerSolution;
+import org.uma.jmetal.solution.integersolution.impl.DefaultIntegerSolution;
+import org.uma.jmetal.util.bounds.Bounds;
 import org.uma.jmetal.util.errorchecking.Check;
-import org.uma.jmetal.util.pseudorandom.PseudoRandomGenerator;
 
 
-public class IntegerBiclusterCrossover implements CrossoverOperator<IntegerCompositeSolution> {
+public class IntegerBiclusterCrossover implements CrossoverOperator<CompositeSolution> {
 
     private double crossoverProbability;
-    private final PseudoRandomGenerator crossoverRandomGenerator;
-    private final PseudoRandomGenerator pointRandomGenerator;
+    private Random crossoverRandomGenerator;
+    private Random pointRandomGenerator;
 
-    public IntegerBiclusterCrossover(double crossoverProbability, PseudoRandomGenerator crossRandomGenerator, PseudoRandomGenerator pointRandomGenerator) {
+    public IntegerBiclusterCrossover(double crossoverProbability) {
+        this(crossoverProbability, new Random(), new Random());
+    }
+
+    public IntegerBiclusterCrossover(double crossoverProbability, Random crossRandomGenerator, Random pointRandomGenerator) {
         this.crossoverProbability = crossoverProbability;
         this.crossoverRandomGenerator = crossRandomGenerator;
         this.pointRandomGenerator = pointRandomGenerator;
     }
 
     @Override
-    public List<IntegerCompositeSolution> execute(List<IntegerCompositeSolution> source) {
+    public List<CompositeSolution> execute(List<CompositeSolution> source) {
         /* Lo que se pasa como parametro es una lista de listas, porque IntegerCompositeSolution
          * a su vez tendrá dos listas de IntegerPermutationSolutions, representando a los genes
          * y a las condiciones respectivamente
@@ -33,49 +39,80 @@ public class IntegerBiclusterCrossover implements CrossoverOperator<IntegerCompo
         Check.that(source.get(0).variables().get(0).variables().size() == source.get(1).variables().get(0).variables().size(), "The parents should have the same size");
         Check.that(source.get(0).variables().get(1).variables().size() == source.get(1).variables().get(1).variables().size(), "The parents should have the same size");
 
-        return doCrossover(this.crossoverProbability, source.get(0), source.get(1));
-    }
-        
-    private List<IntegerCompositeSolution> doCrossover(double probability,
-            IntegerCompositeSolution parent1, IntegerCompositeSolution parent2) {
+        CompositeSolution parent1 = source.get(0);
+        CompositeSolution parent2 = source.get(1);
 
-        List<IntegerCompositeSolution> offspring = new ArrayList<>(2);
-        offspring.add((IntegerCompositeSolution)parent1.copy());
-        offspring.add((IntegerCompositeSolution)parent2.copy());
+        List<CompositeSolution> offspring = new ArrayList<>();
+        offspring.add((CompositeSolution) parent1.copy());
+        offspring.add((CompositeSolution) parent2.copy());
 
-        if (this.crossoverRandomGenerator.nextDouble() <= probability) {
-            // Define the lists that will contain the genes and conditions for the parents
-            List<Integer> parent1Genes = parent1.variables().get(0).variables();
-            List<Integer> parent2Genes = parent2.variables().get(0).variables();
-            List<Integer> parent1Conditions = parent1.variables().get(1).variables();
-            List<Integer> parent2Conditions = parent2.variables().get(1).variables();
+        if (this.crossoverRandomGenerator.nextDouble() <= this.crossoverProbability) {
+            /*
+             * Define the solutions that will contain the genes and conditions for the parents
+             * We will have to access the lists in the solutions using variables() later
+             */
+            IntegerSolution parentGenes1 = (IntegerSolution) parent1.variables().get(0);
+            IntegerSolution parentGenes2 = (IntegerSolution) parent2.variables().get(0);
+            IntegerSolution parentConditions1 = (IntegerSolution) parent1.variables().get(1);
+            IntegerSolution parentConditions2 = (IntegerSolution) parent2.variables().get(1);
 
-            int crossoverPointGenes = this.pointRandomGenerator.nextInt(1, parent1Genes.size() - 1);
-            int crossoverPointConditions = this.pointRandomGenerator.nextInt(1, parent1Conditions.size() - 1);
+            /* To ensure that crossover will happen we establish these indexes in the ints operator */
+            int crossoverPointGenes = this.pointRandomGenerator.ints(1, parentGenes1.variables().size() - 1).findFirst().getAsInt();
+            int crossoverPointConditions = this.pointRandomGenerator.ints(1, parentConditions1.variables().size() - 1).findFirst().getAsInt();
 
-            List<Integer> offspring1Genes = new ArrayList<>()
-                , offspring2Genes = new ArrayList<>(), offspring1Conditions = new ArrayList<>()
-                , offspring2Conditions = new ArrayList<>();
+            List<Integer> offspringGenes1 = new ArrayList<>()
+                , offspringGenes2 = new ArrayList<>(), offspringConditions1 = new ArrayList<>()
+                , offspringConditions2 = new ArrayList<>();
 
-            offspring1Genes.addAll(parent1Genes.subList(0, crossoverPointGenes));
-            offspring1Genes.addAll(parent2Genes.subList(crossoverPointGenes, parent2Genes.size()));
-            offspring1Conditions.addAll(parent1Conditions.subList(0, crossoverPointConditions));
-            offspring1Conditions.addAll(parent2Conditions.subList(crossoverPointConditions, parent2Conditions.size()));
+            offspringGenes1.addAll(parentGenes1.variables().subList(0, crossoverPointGenes));
+            offspringGenes1.addAll(parentGenes2.variables().subList(crossoverPointGenes, parentGenes2.variables().size()));
+            offspringConditions1.addAll(parentConditions1.variables().subList(0, crossoverPointConditions));
+            offspringConditions1.addAll(parentConditions2.variables().subList(crossoverPointConditions, parentConditions2.variables().size()));
 
-            offspring2Genes.addAll(parent2Genes.subList(0, crossoverPointGenes));
-            offspring2Genes.addAll(parent1Genes.subList(crossoverPointGenes, parent1Genes.size()));
-            offspring2Conditions.addAll(parent2Conditions.subList(0, crossoverPointConditions));
-            offspring2Conditions.addAll(parent1Conditions.subList(crossoverPointConditions, parent1Conditions.size()));
+            offspringGenes2.addAll(parentGenes2.variables().subList(0, crossoverPointGenes));
+            offspringGenes2.addAll(parentGenes1.variables().subList(crossoverPointGenes, parentGenes1.variables().size()));
+            offspringConditions2.addAll(parentConditions2.variables().subList(0, crossoverPointConditions));
+            offspringConditions2.addAll(parentConditions1.variables().subList(crossoverPointConditions, parentConditions1.variables().size()));
 
-            offspring.get(0).variables().set(0, new IntegerPermutationSolution(offspring1Genes, parent1.objectives().length, parent1.constraints().length));
-            offspring.get(0).variables().set(1,new IntegerPermutationSolution(offspring1Conditions, parent1.objectives().length, parent1.constraints().length));
-            offspring.get(1).variables().set(0,new IntegerPermutationSolution(offspring2Genes, parent1.objectives().length, parent1.constraints().length));
-            offspring.get(1).variables().set(1, new IntegerPermutationSolution(offspring2Conditions, parent1.objectives().length, parent1.constraints().length));
+            /* We get the gene bounds in order to create the Integersolution that will be stored in the offspring. 
+             * Like this we will ensure that the bounds are preserved from the parents to the offspring.
+             * The bounds in the parent number one have to be the same as the bounds in parent number two for genes.
+             * The same happens with the bounds for the conditions in both parents.
+             */
+            List<Bounds<Integer>> genesBounds = new ArrayList<>();
+            List<Bounds<Integer>> conditionsBounds = new ArrayList<>();
+            for (int i = 0; i < parentGenes1.variables().size(); i++) {
+                genesBounds.add(parentGenes1.getBounds(i));
+            }
+            for (int j = 0; j < parentConditions1.variables().size(); j++) {
+                conditionsBounds.add(parentConditions1.getBounds(j));
+            }
+
+            offspring.get(0).variables().set(0, new DefaultIntegerSolution(genesBounds, parent1.objectives().length, parent1.constraints().length));
+            List<Integer> auxListA = (List<Integer>) offspring.get(0).variables().get(0).variables();
+            auxListA.clear();
+            auxListA.addAll(offspringGenes1);
+
+            offspring.get(0).variables().set(1,new DefaultIntegerSolution(conditionsBounds, parent1.objectives().length, parent1.constraints().length));
+            List<Integer> auxListB = (List<Integer>) offspring.get(0).variables().get(1).variables();
+            auxListB.clear();
+            auxListB.addAll(offspringConditions1);
+
+            offspring.get(1).variables().set(0,new DefaultIntegerSolution(genesBounds, parent1.objectives().length, parent1.constraints().length));
+            List<Integer> auxListC = (List<Integer>) offspring.get(1).variables().get(0).variables();
+            auxListC.clear();
+            auxListC.addAll(offspringGenes2);
+
+            offspring.get(1).variables().set(1, new DefaultIntegerSolution(conditionsBounds, parent1.objectives().length, parent1.constraints().length));
+            List<Integer> auxListD = (List<Integer>) offspring.get(1).variables().get(1).variables();
+            auxListD.clear();
+            auxListD.addAll(offspringConditions2);
 
         }
 
         return offspring;
     }
+
         
     @Override
     public double crossoverProbability() {
