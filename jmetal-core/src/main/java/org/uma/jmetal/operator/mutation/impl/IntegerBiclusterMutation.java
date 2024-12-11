384f355a7a8930377f8a62f7c00dad5019e51783
pmgespina <pmgespina@gmail.com>
2024-12-11 13:27:44 +0000

    Adaptation of mutation operator for the new problem definition

--- jmetal-core/src/main/java/org/uma/jmetal/operator/mutation/impl/IntegerBiclusterMutation.java
+++ jmetal-core/src/main/java/org/uma/jmetal/operator/mutation/impl/IntegerBiclusterMutation.java
@@ -2,14 +2,15 @@ package org.uma.jmetal.operator.mutation.impl;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 import org.uma.jmetal.operator.mutation.MutationOperator;
-import org.uma.jmetal.solution.compositesolution.IntegerCompositeSolution;
-import org.uma.jmetal.solution.permutationsolution.impl.IntegerPermutationSolution;
+import org.uma.jmetal.solution.Solution;
+import org.uma.jmetal.solution.compositesolution.CompositeSolution;
+import org.uma.jmetal.solution.integersolution.IntegerSolution;
 import org.uma.jmetal.util.errorchecking.Check;
-import org.uma.jmetal.util.pseudorandom.PseudoRandomGenerator;
 
-public class IntegerBiclusterMutation implements MutationOperator<IntegerCompositeSolution> {
+public class IntegerBiclusterMutation implements MutationOperator<CompositeSolution> {
 
     /* This will be the mutation that will be applied to Integer Biclusters with the CompositeMutation operator
      * defined in this folder. For each of the IntegerCompositeSolution that we have (representing a bicluster),
@@ -17,11 +18,15 @@ public class IntegerBiclusterMutation implements MutationOperator<IntegerComposi
      */
 
     private double mutationProbability;
-    private final PseudoRandomGenerator randomGeneratorProbability;
-    private final PseudoRandomGenerator randomGeneratorValues;
+    private Random randomGeneratorProbability;
+    private Random randomGeneratorValues;
+
+    public IntegerBiclusterMutation(double probability) {
+        this(probability, new Random(), new Random());
+    }
 
     public IntegerBiclusterMutation(double probability, 
-            PseudoRandomGenerator randomGeneratorProbability, PseudoRandomGenerator randomGeneratorValues) {
+            Random randomGeneratorProbability, Random randomGeneratorValues) {
         this.mutationProbability = probability;
         this.randomGeneratorProbability = randomGeneratorProbability;
         this.randomGeneratorValues = randomGeneratorValues;
@@ -37,26 +42,45 @@ public class IntegerBiclusterMutation implements MutationOperator<IntegerComposi
         this.mutationProbability = probability;
     }
 
+    /*
+     * For future modifications do a package where we store the implementations of mutation operators for rows and columns
+     * They will extend from an interface that will store the common behaviour (the execute method)
+     * In this way we can add more mutation operators simply implementing this interface
+     */
     @Override
-    public IntegerCompositeSolution execute(IntegerCompositeSolution solution) {
+    public CompositeSolution execute(CompositeSolution solution) {
+        Check.notNull(solution);
 
-        List<IntegerPermutationSolution> bicluster = solution.variables();
+        List<Solution<?>> bicluster = solution.variables();
+        IntegerSolution selectedGenes = (IntegerSolution) bicluster.get(0);
+        IntegerSolution selectedConditions = (IntegerSolution) bicluster.get(1);
 
-        for (int i = 0; i < bicluster.size(); i++) {
-            for (int j = 0 ; j < bicluster.get(i).variables().size(); j++) {
-                if (this.randomGeneratorProbability.nextDouble() <= this.mutationProbability) {
-                    if (bicluster.get(i).variables().get(j) == -1) {
-                        /* numero que sea distinto de -1, este en el rango de indices posibles por la matriz, y que no este ya en la lista de indices */
-                        /* para que esté en el rango, cuando definamos el operador de mutacion en el runner del algoritmo definiremos los bounds */
-                        /* no va a generar un -1 porque vamos a establecer que el rango de valores del randomGenerator sea de 0 a numGenes */
-                        while (bicluster.get(i).variables().contains(randomGeneratorValues.nextInt(0, bicluster.get(i).variables().size() - 1))) { }
-                        int value = randomGeneratorValues.nextInt(0, bicluster.get(i).variables().size() - 1);
-                        bicluster.get(i).variables().set(j, value);
-                    } else {
-                        bicluster.get(i).variables().set(j, -1);
-                    }
-                    Collections.sort(bicluster.get(i).variables());
+        for (int i = 0 ; i < selectedGenes.variables().size(); i++) {
+            if (this.randomGeneratorProbability.nextDouble() <= this.mutationProbability) {
+                if (selectedGenes.variables().get(i).equals(-1)) {
+                    /* numero que sea distinto de -1, este en el rango de indices posibles por la matriz, y que no este ya en la lista de indices */
+                    /* para que esté en el rango, cuando definamos el operador de mutacion en el runner del algoritmo definiremos los bounds */
+                    /* no va a generar un -1 porque vamos a establecer que el rango de valores del randomGenerator sea de 0 a numGenes */
+                    while (selectedGenes.variables().contains(randomGeneratorValues.nextInt(selectedGenes.variables().size()))) { }
+                    int value = randomGeneratorValues.nextInt(selectedGenes.variables().size());
+                    selectedGenes.variables().set(i, value);
+                } else {
+                    selectedGenes.variables().set(i, -1);
                 }
+                Collections.sort(selectedGenes.variables());
+            }
+        }
+
+        for (int i = 0 ; i < selectedConditions.variables().size(); i++) {
+            if (this.randomGeneratorProbability.nextDouble() <= this.mutationProbability) {
+                if (selectedConditions.variables().get(i).equals(-1)) {
+                    while (selectedConditions.variables().contains(randomGeneratorValues.nextInt(selectedConditions.variables().size()))) { }
+                    int value = randomGeneratorValues.nextInt(selectedConditions.variables().size());
+                    selectedConditions.variables().set(i, value);
+                } else {
+                    selectedConditions.variables().set(i, -1);
+                }
+                Collections.sort(selectedConditions.variables());
             }
         }
 
